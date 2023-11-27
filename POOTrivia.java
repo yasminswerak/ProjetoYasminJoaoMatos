eec5d055df0189c4bf909114800865ee0f24a33d
Yasmin <132596485+yasminswerak@users.noreply.github.com>
2023-11-27 10:59:54 +0000

    Update POOTrivia.java

--- POOTrivia.java
+++ POOTrivia.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+
 public class POOTrivia {
+    // para sabermos se estamos antes ou depois da terceira pergunta:
+    protected boolean antesTerceiraPergunta;
+    
+    //array com as perguntas e respostas?
+    private ArrayList<Pergunta> perguntas;
+    
+    //preencher array com as perguntas e respostas?
+    public void criarArrayPerguntas(){}
 }
