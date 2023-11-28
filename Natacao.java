2f2c9344a323632c7389671dfca445f358d2f698
Yasmin <132596485+yasminswerak@users.noreply.github.com>
2023-11-28 18:40:03 +0000

    Update Natacao.java

--- Natacao.java
+++ Natacao.java
@@ -1,2 +1,37 @@
+/**
+ * Classe publica Natacao filha de Desporto
+ */
 public class Natacao extends Desporto{
+    /**
+     * Construtor vazio de Natacao
+     */
+    public Natacao() {}
+
+    /**
+     * Construtor de Natacao
+     * @param enunciado Enunciado da pergunta
+     * @param resposta Resposta da pergunta
+     */
+    public Natacao(String enunciado, String resposta) {
+        super(enunciado, resposta);
+    }
+
+    @Override
+    public String[] getOpcoesFacil() {
+        return super.opcoes;
+    }
+
+    @Override
+    public String[] getOpcoesDificil() {
+        return super.opcoes;
+    }
+
+    /**
+     * Override para obter o valor/pontuacao da pergunta
+     * @return Pontuacao da pergunta
+     */
+    @Override
+    public int valorPergunta() {
+        return super.valorPergunta() + 10;
+    }
 }
