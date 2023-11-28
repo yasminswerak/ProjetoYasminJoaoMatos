a0a3b9f93f1b9f810d1aba56e91f81f1438d64d2
Yasmin <132596485+yasminswerak@users.noreply.github.com>
2023-11-28 18:41:40 +0000

    Update Ski.java

--- Ski.java
+++ Ski.java
@@ -1,2 +1,23 @@
 public class Ski extends Desporto{
+
+    public Ski() {}
+
+    public Ski(String enunciado, String resposta) {
+        super(enunciado, resposta);
+    }
+
+    @Override
+    public String[] getOpcoesFacil() {
+        return super.opcoes;
+    }
+
+    @Override
+    public String[] getOpcoesDificil() {
+        return super.opcoes;
+    }
+
+    @Override
+    public int valorPergunta() {
+        return super.valorPergunta() * 2;
+    }
 }
