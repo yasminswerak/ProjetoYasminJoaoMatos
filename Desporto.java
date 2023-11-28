c9b615071ef161da8b456f05155e7020f3a80536
Yasmin <132596485+yasminswerak@users.noreply.github.com>
2023-11-28 18:38:42 +0000

    Update Desporto.java

--- Desporto.java
+++ Desporto.java
@@ -1,2 +1,34 @@
-public class Desporto extends Pergunta{
+/**
+ * Classe abstrata Desporto filha de Pergunta
+ */
+public abstract class Desporto extends Pergunta{
+    /**
+     * Construtor vazio de Desporto
+     */
+    public Desporto() {}
+
+    /**
+     * Construtor de Desporto
+     * @param enunciado enunciado da pergunta
+     */
+    public Desporto(String enunciado) {
+        super(enunciado);
+    }
+
+    /**
+     * Construtor de Desporto
+     * @param enunciado enunciado da pergunta
+     * @param resposta resposta da pergunta
+     */
+    public Desporto(String enunciado, String resposta){
+        super(enunciado, resposta, new String[]{"Verdadeiro", "Falso"});
+    }
+    /**
+     * Override para obter o valor/pontuacao da pergunta
+     * @return pontuacao da pergunta
+     */
+    @Override
+    public int valorPergunta() {
+        return super.valorPergunta() + 3;
+    }
 }
