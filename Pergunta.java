7ce90995520b96fe0924050c2d26c9c1dfdfff69
Yasmin <132596485+yasminswerak@users.noreply.github.com>
2023-11-28 18:41:17 +0000

    Update Pergunta.java

--- Pergunta.java
+++ Pergunta.java
@@ -1,2 +1,100 @@
-public class Pergunta {
+/**
+ * Classe abstrata Pergunta
+ */
+public abstract class Pergunta {
+    //atributos:
+    /**
+     * Atributos String enunciado e resposta
+     */
+    protected String enunciado, resposta;
+    /**
+     * Atributo int pontos
+     */
+    protected int pontos;
+    /**
+     * Atributo array opcoes
+     */
+    protected String[] opcoes;
+
+    //construtores:
+    /**
+     * Contrutor vazio de Pergunta
+     */
+    public Pergunta() {}
+
+    /**
+     * Construtor de Pergunta
+     * @param enunciado Enunciado da pergunta
+     */
+    public Pergunta(String enunciado) {
+        this.enunciado = enunciado;
+        this.pontos = 5;
+    }
+
+    /**
+     * Construtor de Pergunta
+     * @param enunciado Enunciado da pergunta
+     * @param resposta Resposta da pergunta
+     * @param opcoes Opcoes
+     */
+    public Pergunta(String enunciado, String resposta, String[] opcoes) {
+        this.enunciado = enunciado;
+        this.pontos = 5;
+        this.resposta = resposta;
+        this.opcoes = opcoes;
+    }
+
+    //getters e setters:
+
+    /**
+     * Getter de Enunciado
+     * @return Enunciado
+     */
+    public String getEnunciado() {
+        return enunciado;
+    }
+
+    /**
+     * Setter de Enunciado
+     * @param enunciado Enunciado
+     */
+    public void setEnunciado(String enunciado) {
+        this.enunciado = enunciado;
+    }
+
+    /**
+     * Getter de Resposta
+     * @return Resposta
+     */
+    public String getResposta() {
+        return resposta;
+    }
+
+    /**
+     * Setter de Resposta
+     * @param resposta Resposta
+     */
+    public void setResposta(String resposta) {
+        this.resposta = resposta;
+    }
+
+    /**
+     * Funcao abstrata
+     * @return opcoes de perguntas faceis
+     */
+    public abstract String[] getOpcoesFacil();
+
+    /**
+     * Funcao abstrata
+     * @return opcoes de perguntas dificeis
+     */
+    public abstract String[] getOpcoesDificil();
+
+    /**
+     * Funcao que determina o valor de cada pergunta
+     * @return valor base da pergunta (5)
+     */
+    public int valorPergunta(){
+        return pontos; //Valor basico da pergunta
+    }
 }
