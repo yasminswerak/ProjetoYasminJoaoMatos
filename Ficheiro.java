9a331291941c440aefbc1be5e687d918cfe2ef87
Yasmin <132596485+yasminswerak@users.noreply.github.com>
2023-11-28 18:39:06 +0000

    Update Ficheiro.java

--- Ficheiro.java
+++ Ficheiro.java
@@ -1,1 +1,25 @@
+import java.util.ArrayList;
 
+/**
+ * Classe publica Ficheiro
+ */
+public class Ficheiro {
+    /**
+     * Metodo para ler as perguntas do ficheiro texto
+     * @return
+     */
+    public ArrayList<Pergunta> lerPerguntas(){
+        ArrayList<Pergunta> perguntas = new ArrayList<Pergunta>();
+        //ler primeira linha
+        //switch(linha[0])
+        //case 0 -> perguntas.add(new Arte(enunciado, resposta, opcoes));
+        //case 1 -> Ciencias...
+        return perguntas;
+    }
+
+    public ArrayList<Jogo> lerJogos(){
+        ArrayList<Jogo> jogos = new ArrayList<Jogo>();
+        //Ler do ficheiro de objetos
+        return jogos;
+    }
+}
